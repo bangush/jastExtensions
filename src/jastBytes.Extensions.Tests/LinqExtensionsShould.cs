511a590fb25c9cb7313dbc4c897451e7fc781d59
Jan Steffen <jan.steffen@advision-digital.de>
2018-10-09 15:25:19 +0200

    Add test for linq extension

---
+++ src/jastBytes.Extensions.Tests/LinqExtensionsShould.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace jastBytes.Extensions.Tests
+{
+    public class LinqExtensionsShould
+    {
+        [Fact]
+        public void ForEach()
+        {
+            var listOfExecuted = new List<int>();
+            var enumerable = Enumerable.Range(0, 1000);
+
+            var enumerated = enumerable
+                .Skip(1)
+                .Take(2)
+                .ForEach(i => listOfExecuted.Add(i))
+                .ToList();
+
+            Assert.True(listOfExecuted.Count == 2);
+            Assert.Equal(enumerated, listOfExecuted);
+        }
+    }
+}
