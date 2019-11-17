3757818108b671cc3c26f2bb14eaf2b689594375
Jan Steffen <jan.steffen@posteo.de>
2019-11-17 14:27:02 +0100

    Add golang like error handling

---
+++ jastBytes.Extensions.ErrorHandling/ErrorHandling.cs
@@ -0,0 +1,104 @@
+﻿// MIT License
+// 
+// Copyright (c) 2018 Jan Steffen
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using System;
+using System.Threading.Tasks;
+
+namespace jastBytes.Extensions.ErrorHandling
+{
+    public static class ErrorHandling
+    {
+        public static Exception Err(Action action)
+        {
+            try
+            {
+                action.Invoke();
+                return null;
+            }
+            catch (Exception ex)
+            {
+                return ex;
+            }
+        }
+
+        public static async Task<Exception> ErrAsync(Func<Task> func)
+        {
+            try
+            {
+                await func.Invoke();
+                return null;
+            }
+            catch (Exception ex)
+            {
+                return ex;
+            }
+        }
+
+        public static (TResult, Exception) Err<TResult>(Func<TResult> func)
+        {
+            try
+            {
+                return (func.Invoke(), null);
+            }
+            catch (Exception ex)
+            {
+                return (default, ex);
+            }
+        }
+
+        public static async Task<(TResult, Exception)> ErrAsync<TResult>(Func<Task<TResult>> func)
+        {
+            try
+            {
+                return (await func.Invoke(), null);
+            }
+            catch (Exception ex)
+            {
+                return (default, ex);
+            }
+        }
+
+        public static (TResult, TException) Err<TResult, TException>(Func<TResult> func) where TException : Exception
+        {
+            try
+            {
+                return (func.Invoke(), null);
+            }
+            catch (TException ex)
+            {
+                return (default, ex);
+            }
+        }
+
+        public static async Task<(TResult, TException)> ErrAsync<TResult, TException>(Func<Task<TResult>> func) where TException : Exception
+        {
+            try
+            {
+                return (await func.Invoke(), null);
+            }
+            catch (TException ex)
+            {
+                return (default, ex);
+            }
+        }
+    }
+}
---
+++ src/jastBytes.Extensions.Tests/ErrorHandlingExtensionsShould.cs
@@ -0,0 +1,97 @@
+﻿// MIT License
+// 
+// Copyright (c) 2018 Jan Steffen
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using System;
+using System.Threading.Tasks;
+using Xunit;
+using static jastBytes.Extensions.ErrorHandling.ErrorHandling;
+
+namespace jastBytes.Extensions.Tests
+{
+    public class ErrorHandlingExtensionsShould
+    {
+        [Fact]
+        public void HandleExceptionWithReturn()
+        {
+            var (str, err) = Err(() => CanErrorWithResult(true));
+            Assert.Null(str);
+            Assert.NotNull(err);
+
+            (str, err) = Err(() => CanErrorWithResult(false));
+            Assert.NotNull(str);
+            Assert.Null(err);
+
+            Assert.Throws<Exception>(() => Err<string, ArgumentException>(() => CanErrorWithResult(true)));
+        }
+
+        [Fact]
+        public async Task HandleExceptionWithReturnAsync()
+        {
+            var (str, err) = await ErrAsync(() => CanErrorWithResultAsync(true));
+            Assert.Null(str);
+            Assert.NotNull(err);
+
+            await Assert.ThrowsAsync<Exception>(() => ErrAsync<string, ArgumentException>(() => CanErrorWithResultAsync(true)));
+        }
+
+        [Fact]
+        public void HandleExceptionWithoutReturn()
+        {
+            var err = Err(() => CanErrorNoResult(true));
+            Assert.NotNull(err);
+
+            err = Err(() => CanErrorNoResult(false));
+            Assert.Null(err);
+        }
+
+        [Fact]
+        public async Task HandleExceptionWithoutReturnAsync()
+        {
+            var err = await ErrAsync(() => CanErrorNoResultAsync(true));
+            Assert.NotNull(err);
+
+            err = await ErrAsync(() => CanErrorNoResultAsync(false));
+            Assert.Null(err);
+        }
+
+        private static async Task CanErrorNoResultAsync(bool shouldThrow)
+        {
+            await Task.Factory.StartNew(() => CanErrorNoResult(shouldThrow));
+        }
+
+        private static async Task<string> CanErrorWithResultAsync(bool shouldThrow)
+        {
+            return await Task.Factory.StartNew(() => CanErrorWithResult(shouldThrow));
+        }
+
+        private static void CanErrorNoResult(bool shouldThrow)
+        {
+            if (shouldThrow) throw new Exception("Oh snap!");
+        }
+
+        private static string CanErrorWithResult(bool shouldThrow)
+        {
+            if (shouldThrow) throw new Exception("Oh snap!");
+            return "Hello World";
+        }
+    }
+}
